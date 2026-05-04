e921b571d5d44b36af2c868cc4e5e21019a94c6b
Abdelhadi <76612137+abdelhadi-haddadi@users.noreply.github.com>
2026-05-04 12:59:27 +0100

    Update footer with current year and new developer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,17 +2,19 @@ import { Card, CardContent, CardTitle } from "./ui/card";
 import Link from "next/link";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  
   return (
     <Card className="rounded-none bg-opacity-80 border-none outline outline-[0.1px] md:px-0 md:mx-auto outline-gray-100/40 ">
       <CardContent className="flex flex-col gap-2 md:flex-row md:justify-between py-6 items-center  ">
         <CardTitle className="flex md:justify-center gap-x-1 font-normal">
-          © 2024 Axis Buddy. All Rights Reserved.
+          © {currentYear} QA Engine. All Rights Reserved.
         </CardTitle>
 
         <CardTitle className="flex md:justify-center gap-x-1 font-normal">
           Design and developed by
-          <Link href={"https://sahilkumardev.netlify.app"} target="_blank">
-            <span className="text-chai font-semibold">Sahil Kumar Dev</span>
+          <Link href={"https://abdelhadi-portfolio.netlify.app"} target="_blank">
+            <span className="text-chai font-semibold">Abdelhadi HADDADI</span>
           </Link>
         </CardTitle>
       </CardContent>
